70e7e20be144788147a1d9ae1705971fc1b7f977
ashouryoussef <62855014+ashouryoussef@users.noreply.github.com>
2020-04-11 17:38:58 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    TOKEN: 'Njk3OTg4NTYwNDM0ODg4Nzk5.Xo_Shw.DDRT2ZW2yx3D6_p3hXymobcbuvM',    
+    YT_API_KEY: 'api ',
+    prefix: '#',
+    devs:  ['451562739056377866']
+}
+module.exports = {
+    TOKEN: 'Njk3OTg4NTYwNDM0ODg4Nzk5.Xo_Shw.DDRT2ZW2yx3D6_p3hXymobcbuvM',    
+    YT_API_KEY: 'api ',
+    prefix: '#',
+    devs:  ['584130594959654952']
+}
